4f3475924d3061165bd0c51115e41ed95664096e
ACarey258 <andrea.carey@dfw.wa.gov>
2017-06-28 10:12:58 -0700

    transposed data from wide to long

--- 2016Blackmouth_Scatterplots.R
+++ 2016Blackmouth_Scatterplots.R
@@ -31,24 +31,53 @@ levels(BMmuscle$MA) #examine levels of MA = alphabetical, need to be from lowest
 BMmuscle$MA <- factor(BMmuscle$MA, levels = c('MA6', 'MA7', 'MA8.1', 'MA8.2', 'MA9', 'MA10', 'MA12')) #redefine the order of levels
 
 ###
-# 1. Scatterplot of individual lengths (y) and ages (x)
+# 1. Scatterplot of individual lengths (y) and scale ages - Gilbert Rich (x)
 
 a <- ggplot(BMmuscle, aes(x = ScaleAge, y = ForkLength)) + geom_point()
 print(a) #simple scatterplot, removed NAs (13)
 print({a + aes(colour = MA)}) #color codes fish based on marine area where they were collected, removed NAs (13) 
 
-a <- a + aes(color = MA)
+a <- a + aes(color = MA) #color codes fish based on marine area where they were collected, removed NAs (13) 
 
 # Save the plot to outfile
-ggsave(paste(outfile,"scaleplot.jpg",sep=""),a,height=5,width=7.5)
+ggsave(paste(outfile,"16BM_ScaleAge and Length.jpg",sep=""),a,height=5,width=7.5)
 
-## need to export graphic...
+# 2. Scatterplot of individual lengths (y) and saltwater ages (x)
+b <- ggplot(BMmuscle, aes(x = SWAge, y = ForkLength)) + geom_point()
+b <- b + aes(colour = MA) 
 
-# default dimensions in pixels, copied from website
-jpeg(file = "BM_length and age.jpg",
-     width = 10,
-     height = 6.67, 
-     units = "mm",
-     pointsize = 12, 
-     quality = 75)
-dev.off()
+ggsave(paste(outfile,"16BM_SWage and Length.jpg",sep=""),b,height=5,width=7.5)
+
+###
+# Setting all data up for plotting
+
+#create new dataframe from master data file - BM, keep only muscle samples, remove any rows with unknowns for ScaleAge n = 13
+colnames(BM)
+BMdata <- as.data.frame(BM[ ,c(2:4,6:11,13:18,20:26)])
+BMdata <- subset(BMdata, Matrix=="muscle") #keep only muscle samples, don't need whole body samples since we only have them from MA 10
+BMmus.all <- subset(BMdata, Fwage>="1") #removes fish that did not get analyzed for POPs (priority 3 fish which were unable to be aged)
+
+#add MA to each Marine Area # so that R reads it as a categorical variable and not a continuous variable
+BMmus.all$MA[BMmus.all$MarineArea == 6] <- "MA6"
+BMmus.all$MA[BMmus.all$MarineArea == 7] <- "MA7"
+BMmus.all$MA[BMmus.all$MarineArea == 8.1] <- "MA8.1"
+BMmus.all$MA[BMmus.all$MarineArea == 8.2] <- "MA8.2"
+BMmus.all$MA[BMmus.all$MarineArea == 9] <- "MA9"
+BMmus.all$MA[BMmus.all$MarineArea == 10] <- "MA10"
+BMmus.all$MA[BMmus.all$MarineArea == 12] <- "MA12"
+BMmus.all$MA <-factor(BMmus.all$MA) #assign MA column as a factor or categorical variable
+levels(BMmus.all$MA) #examine levels of MA = alphabetical, need to be from lowest to highest
+BMmus.all$MA <- factor(BMmus.all$MA, levels = c('MA6', 'MA7', 'MA8.1', 'MA8.2', 'MA9', 'MA10', 'MA12')) #redefine the order of levels
+
+#create new dataframe, remove MarineArea column since it's a continuous variable and remove Matrix since it's useless
+
+BMpops <- as.data.frame(BMmus.all[ ,c(1,4:23)])
+
+# transpose data from wide format to long format
+BMpops.long <- reshape(BMpops,varying = c("ForkLength", "ScaleAge", "SWAge", "Fwage", "OutmigrationLH", "Origin", "Lipids", 
+                                            "HCB", "SumHCHs", "SumCHLDs", "dieldrin", "SumDDTs", "TPCBs", "SumBDE", "NOAA_CNratio", 
+                                            "NOAA_delta13C_LipExt", "NOAA_delta15N_LipExt", "UW_DeltaN_NotExt","UW_DeltaC_NotExt"), 
+                        times = c("ForkLength", "ScaleAge", "SWAge", "Fwage", "OutmigrationLH", "Origin", "Lipids", 
+                                  "HCB", "SumHCHs", "SumCHLDs", "dieldrin", "SumDDTs", "TPCBs", "SumBDE", "NOAA_CNratio", 
+                                  "NOAA_delta13C_LipExt", "NOAA_delta15N_LipExt", "UW_DeltaN_NotExt","UW_DeltaC_NotExt"), 
+                        v.names = "Variables", idvar = c("FishID", "MA"), direction = "long")
