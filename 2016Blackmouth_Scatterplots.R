86f0ed13be7798ff94d2f7b71222ebf366f3693b
ACarey258 <andrea.carey@dfw.wa.gov>
2017-07-06 14:07:33 -0700

    lots changed
    How do you get the legend symbols to match the actual plots?

--- 2016Blackmouth_Scatterplots.R
+++ 2016Blackmouth_Scatterplots.R
@@ -20,9 +20,13 @@ outfile = paths[[2]]
 BM <- read_excel(paths[[1]],"BlackmouthPOPsSIs")
 
 #create dataframe of data only needed for scatterplots, subset out the Matrix = muscle
-BMdat <- as.data.frame(BM[ ,c(2:4,6:27)])
-BMmuscle <- subset(BMdat, Matrix=="muscle")
-BMmuscle$ForkLength <- as.numeric(BMmuscle$ForkLength)
+BMdat <- as.data.frame(BM[ ,c(2:4,6:30)])
+BMmuscle <- subset(BMdat, Matrix =="muscle") #includes 13 priority 3 fish that weren not analyzed for POPs
+BMmuscle$FL_cm <- as.numeric(BMmuscle$FL_cm)  #make lengths and weights a numeric variable since there's one NA in there
+BMmuscle$TL_cm <- as.numeric(BMmuscle$TL_cm)
+BMmuscle$TL_inch <- as.numeric(BMmuscle$TL_inch)
+BMmuscle$WBWt_lbs <- as.numeric(BMmuscle$WBWt_lbs)
+BMmuscle$Wt_kg <- as.numeric(BMmuscle$Wt_kg)
 
 #add MA to Marine Area numbers in order to change data from a continuous variable to a categorical variable
 BMmuscle$MA[BMmuscle$MarineArea == 6] <- "MA6"
@@ -39,7 +43,7 @@ BMmuscle$MA <- factor(BMmuscle$MA, levels = c('MA6', 'MA7', 'MA8.1', 'MA8.2', 'M
 ###
 # 1. Scatterplot of individual lengths (y) and scale ages - Gilbert Rich (x)
 
-a <- ggplot(BMmuscle, aes(x = ScaleAge, y = ForkLength)) + geom_point()
+a <- ggplot(BMmuscle, aes(x = ScaleAge, y = FL_cm)) + geom_point()
 print(a) #simple scatterplot, removed NAs (13)
 print({a + aes(colour = MA)}) #color codes fish based on marine area where they were collected, removed NAs (13) 
 
@@ -49,7 +53,7 @@ a <- a + aes(color = MA) #color codes fish based on marine area where they were 
 ggsave(paste(outfile,"16BM_ScaleAge and Length.jpg",sep=""),a,height=5,width=7.5)
 
 # 2. Scatterplot of individual lengths (y) and saltwater ages (x)
-b <- ggplot(BMmuscle, aes(x = SWAge, y = ForkLength)) + geom_point()
+b <- ggplot(BMmuscle, aes(x = SWAge, y = FL_cm)) + geom_point()
 b <- b + aes(colour = MA) 
 print(b)
 ggsave(paste(outfile,"16BM_SWage and Length.jpg",sep=""),b,height=5,width=7.5)
@@ -64,82 +68,107 @@ BMmuscle$SWageCat <-factor(BMmuscle$SWageCat) #assign SWAge column as a factor/c
 levels(BMmuscle$SWageCat) #examine levels of SWageCat = alphabetical, need to be from lowest to highest
 BMmuscle$SWageCat <- factor(BMmuscle$SWageCat, levels = c('Zero', 'One', 'Two', 'Three', 'Four'))
 
-lw_eqn <- function(BMmuscle){
-    m <- lm(WholeFishWt ~ ForkLength, BMmuscle);
-    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                     list(a = format(coef(m)[1], digits = 2), 
-                          b = format(coef(m)[2], digits = 2), 
-                          r2 = format(summary(m)$r.squared, digits = 3)))
-    as.character(as.expression(eq));                 
-} #function to add regression equation and r squared to a scatterplot
 
-lw <- ggplot(BMmuscle, aes(x = ForkLength, y = WholeFishWt)) + 
-    geom_point(aes(color = SWageCat), shape = 1, size = 3) + #allows for only one regression line for all points instead of by category
-    geom_smooth(method = "lm",
-                se = FALSE,
-                color = "black") + #add regression line, need to add r^2 for each line
+#Log transform length and weight data 
+lwdat <- as.data.frame(BMmuscle[ ,c(1:2,4:15,29:30)])
+lwdat <- transform(lwdat, log_FLcm = log10(FL_cm))
+lwdat <- transform(lwdat, log_TLcm = log10(TL_cm))
+lwdat <- transform(lwdat, log_TLinch = log10(TL_inch))
+lwdat <- transform(lwdat, log_WTlbs = log10(WBWt_lbs))
+lwdat <- transform(lwdat, log_WTkg = log10(Wt_kg))
+
+# try simple plotting to see how data looks before getting fancy with ggplot
+LenWt <- plot(lwdat$TL_inch, lwdat$WBWt_lbs,
+              xlab = "Total Length (inches)",
+              ylab = "Whole Body Weight (pounds)",
+              log = "x") #log x=axis scale
+
+# ggplot scatterplot 
+lw <- ggplot(lwdat, aes(x = log_TLinch, y = WBWt_lbs)) + 
+    geom_point(aes(color = SWageCat), size = 3) +
+    geom_point(shape = 1, size = 3, color = "black") + #allows for only one regression line for all points instead of by category
     theme_classic() +
-    geom_text(x = 45, y = 10, label = lw_eqn(BMmuscle), parse = TRUE) + #adds regression equation and r^2 value
-    labs(x = "Fork Length (cm)", y = "Whole Body Fish Weight (lbs)", color = "SW Age")
-
+    labs(x = "(Log10) Total Length (in)", y = "Whole Body Fish Weight (lbs)", color = "SW Age")
 print(lw)
-ggsave(paste(outfile,"16BM_LengthWtRegression.jpg",sep=""),lw,height = 5,width = 7.5)
 
-## START HERE
-##BMmuscle$WholeFishWtCat <- categorize(BMmuscle$WholeFishWt, breaks = 4, quantile = FALSE,
-                                      #labels = c("one", "two", "three", "four"))
-##levels(BMmuscle$WholeFishWtCat)
+#explore which line is best for data
+lw.all <- lw + stat_smooth(method = "lm", formula = y ~ x, size = 1, se = FALSE, colour = "black") + #linear fit
+    stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1, se = FALSE, colour = "blue") + #quadratic, includes squared x term
+    stat_smooth(method = "loess", formula = y ~ x, size = 1, se = FALSE, colour = "red") + #locally weighted regression
+    stat_smooth(method = "gam", formula = y ~ s(x), size = 1, se = FALSE, colour = "green") + # a GAM w/a smoother for x
+    stat_smooth(method = "gam", formula = y ~ s(x, k = 3), size = 1, se = FALSE, colour = "violet") # a GAM w/an x smoother and a dimension of 3 
+print(lw.all)
 
-###
-# Setting all data up for plotting
+# scatter with best looking line - gam = general additive model, smoother on the x predictor variable
+lw2 <- lw + stat_smooth(method = "gam", formula = y ~ s(x), size = 1, colour = "black")
+print(lw2) #gray shading is standard error
+
+ggsave(paste(outfile,"16BM_LengthWt_.jpg",sep=""),lw2,height = 5,width = 7.5)
+
+# code for calculating regression equation and r squared values - not used since length vs. weight relationship isn't linear
+#lw_eqn <- function(BMmuscle){
+#m <- lm(WholeFishWt ~ ForkLength, BMmuscle);
+#eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+#                list(a = format(coef(m)[1], digits = 2), 
+#                    b = format(coef(m)[2], digits = 2), 
+#                   r2 = format(summary(m)$r.squared, digits = 3)))
+#    as.character(as.expression(eq));                 
+#} #function to add regression equation and r squared to a scatterplot
+
+# text for adding regression eqtn and r^2 value to plot: geom_text(x = 45, y = 10, label = lw_eqn(BMmuscle), parse = TRUE) 
+
+
+
+##############################################################################################################################################
+# SETTING UP ALL DATA FOR PLOTTING IN A LOOP
+# DATA NEEDS TO BE IN SKINNY/LONG FORMAT
 
 #create new dataframe from master data file - BM, keep only muscle samples, remove any rows with unknowns for ScaleAge n = 13
 colnames(BM)
-BMdata <- as.data.frame(BM[ ,c(2:4,6:11,13:18,20:26)])
+BMdata <- as.data.frame(BM[ ,c(2:4,6:15,17:22,24:30)])
 BMdata <- subset(BMdata, Matrix=="muscle") #keep only muscle samples, don't need whole body samples since we only have them from MA 10
-BMmus.all <- subset(BMdata, Fwage>="1") #removes fish that did not get analyzed for POPs (priority 3 fish which were unable to be aged)
+BMmus.chem <- subset(BMdata, FWAge>="1") #removes fish that did not get analyzed for POPs (priority 3 fish which were unable to be aged)
 
 #add MA to each Marine Area # so that R reads it as a categorical variable and not a continuous variable
-BMmus.all$MA[BMmus.all$MarineArea == 6] <- "MA6"
-BMmus.all$MA[BMmus.all$MarineArea == 7] <- "MA7"
-BMmus.all$MA[BMmus.all$MarineArea == 8.1] <- "MA8.1"
-BMmus.all$MA[BMmus.all$MarineArea == 8.2] <- "MA8.2"
-BMmus.all$MA[BMmus.all$MarineArea == 9] <- "MA9"
-BMmus.all$MA[BMmus.all$MarineArea == 10] <- "MA10"
-BMmus.all$MA[BMmus.all$MarineArea == 12] <- "MA12"
-BMmus.all$MA <-factor(BMmus.all$MA) #assign MA column as a factor or categorical variable
-levels(BMmus.all$MA) #examine levels of MA = alphabetical, need to be from lowest to highest
-BMmus.all$MA <- factor(BMmus.all$MA, levels = c('MA6', 'MA7', 'MA8.1', 'MA8.2', 'MA9', 'MA10', 'MA12')) #redefine the order of levels
+BMmus.chem$MA[BMmus.chem$MarineArea == 6] <- "MA6"
+BMmus.chem$MA[BMmus.chem$MarineArea == 7] <- "MA7"
+BMmus.chem$MA[BMmus.chem$MarineArea == 8.1] <- "MA8.1"
+BMmus.chem$MA[BMmus.chem$MarineArea == 8.2] <- "MA8.2"
+BMmus.chem$MA[BMmus.chem$MarineArea == 9] <- "MA9"
+BMmus.chem$MA[BMmus.chem$MarineArea == 10] <- "MA10"
+BMmus.chem$MA[BMmus.chem$MarineArea == 12] <- "MA12"
+BMmus.chem$MA <-factor(BMmus.chem$MA) #assign MA column as a factor or categorical variable
+levels(BMmus.chem$MA) #examine levels of MA = alphabetical, need to be from lowest to highest
+BMmus.chem$MA <- factor(BMmus.chem$MA, levels = c('MA6', 'MA7', 'MA8.1', 'MA8.2', 'MA9', 'MA10', 'MA12')) #redefine the order of levels
 
 #create new dataframe, remove MarineArea column since it's a continuous variable and remove Matrix since it's useless
-
-BMpops <- as.data.frame(BMmus.all[ ,c(1,4:23)])
+colnames(BMmus.chem)
+BMpops <- as.data.frame(BMmus.chem[ ,c(1,4:27)])
 
 # transpose data from wide format to long format
-BMpops.long <- reshape(BMpops,varying = c("ForkLength", "ScaleAge", "SWAge", "Fwage", "OutmigrationLH", "Origin", "Lipids", 
+BMpops.long <- reshape(BMpops,varying = c("FL_cm", "TL_cm", "TL_inch", "WBWt_lbs", "Wt_kg", "ScaleAge", "SWAge", "FWAge", "Lipids", 
                                             "HCB", "SumHCHs", "SumCHLDs", "dieldrin", "SumDDTs", "TPCBs", "SumBDE", "NOAA_CNratio", 
                                             "NOAA_delta13C_LipExt", "NOAA_delta15N_LipExt", "UW_DeltaN_NotExt","UW_DeltaC_NotExt"), 
-                        times = c("ForkLength", "ScaleAge", "SWAge", "Fwage", "OutmigrationLH", "Origin", "Lipids", 
+                        times = c("FL_cm", "TL_cm", "TL_inch", "WBWt_lbs", "Wt_kg", "ScaleAge", "SWAge", "FWAge", "Lipids", 
                                   "HCB", "SumHCHs", "SumCHLDs", "dieldrin", "SumDDTs", "TPCBs", "SumBDE", "NOAA_CNratio", 
                                   "NOAA_delta13C_LipExt", "NOAA_delta15N_LipExt", "UW_DeltaN_NotExt","UW_DeltaC_NotExt"), 
-                        v.names = "Variables", idvar = c("FishID", "MA"), direction = "long")
+                        v.names = "Variables", idvar = c("FishID", "OutmigrationLH", "Origin", "MA"), direction = "long")
 
-# scatterplot of length vs. TPCBs colored by Marine Area, all fish
-PCBslen <- ggplot(BMpops, aes(x = ForkLength, y = TPCBs)) + geom_point()
+## Using the wide format data set - scatterplot of length vs. TPCBs colored by Marine Area, all fish
+PCBslen <- ggplot(BMpops, aes(x = FL_cm, y = TPCBs)) + geom_point()
 PCBslen <- PCBslen + aes(colour = MA)
-
 print(PCBslen)
 
-PCBslreg <- ggplot(BMpops, aes(x = ForkLength, y = TPCBs, color = MA)) + 
+PCBslreg <- ggplot(BMpops, aes(x = FL_cm, y = TPCBs, color = MA)) + 
     geom_point() +
     geom_smooth(method = "lm", #adds regression lines to each Marine Area 
-                se = FALSE) #removes shaded standard error? area around regression lines
+                se = FALSE) #removes shaded standard error area around regression lines
 print(PCBslreg)
 ggsave(paste(outfile,"16BM_LengthTPCBs.jpg",sep=""),PCBslreg,height=5,width=7.5)
 
 ## only plot legal sized fish > 56cm Total Length, 52.5 cm FL = 22 inch fish, Graphed fish that were greater than ~51.5cm Fork Length
-BMlegal <- subset(BMpops, ForkLength>="51.5") #excludes 2 fish from MA12, 1 from MA7 and 6 from MA10
-PCBsleg <- ggplot(BMlegal, aes(x = ForkLength, y = TPCBs, color = MA)) + 
+BMlegal <- subset(BMpops, TL_inch>="21.5") #excludes 1 fish from MA12, 1 from MA7 and 6 from MA10
+PCBsleg <- ggplot(BMlegal, aes(x = FL_cm, y = TPCBs, color = MA)) + 
     geom_point() +
     geom_smooth(method = "lm",
                 se = FALSE)
@@ -148,9 +177,10 @@ ggsave(paste(outfile,"16BM_LengthTPCBs_LegalSize.jpg",sep=""),PCBsleg,height=5,w
 
 ## messing around with ggplot...
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") #palette of 8 colorblind-friendly colors
-#excludes 2 fish from MA12, 1 from MA7 and 6 from MA10
-PCBsleg2 <- ggplot(BMlegal, aes(x = ForkLength, y = TPCBs, color = MA)) + 
-    geom_point(size = 3) +
+#excludes 1 fish from MA12, 1 from MA7 and 6 from MA10
+PCBsleg2 <- ggplot(BMlegal, aes(x = FL_cm, y = TPCBs, color = MA)) + 
+    geom_point(aes(color = MA), size = 3) +
+    geom_point(shape = 1, size = 3, color = "black") +
     scale_colour_manual(values = cbPalette) + #colorblind-friendly colors
     geom_smooth(method = "lm",
                 se = FALSE) + #adds regression lines, need to add r^2 for each line
@@ -164,9 +194,9 @@ print(PCBsleg2)
 ggsave(paste(outfile,"16BM_LengthTPCBs_LegalSize_Ver2.jpg",sep=""),PCBsleg2,height=5,width=7.5)
 
 ## PBDEs vs length
-
-PBDEsleg <- ggplot(BMlegal, aes(x = ForkLength, y = SumBDE, color = MA)) + 
-    geom_point(size = 3) +
+PBDEsleg <- ggplot(BMlegal, aes(x = FL_cm, y = SumBDE, color = MA)) + 
+    geom_point(aes(color = MA), size = 3) +
+    geom_point(shape = 1, size = 3, color = "black") +
     scale_colour_manual(values = cbPalette) + #colorblind-friendly colors
     geom_smooth(method = "lm",
                 se = FALSE) + #adds regression lines, need to add r^2 for each line
@@ -178,11 +208,12 @@ PBDEsleg <- ggplot(BMlegal, aes(x = ForkLength, y = SumBDE, color = MA)) +
 print(PBDEsleg)
 ggsave(paste(outfile,"16BM_LengthPBDEs_LegalSize_Ver2.jpg",sep=""),PBDEsleg,height=5,width=7.5)
 
+
 ## !! TO DO - add an r-squared value for each regression line/Marine Area
 
 ## Comparison of contaminants and various biometrics (ForkLength, ScaleAge, SWage, FWage, OutmigrationLH, Lipids)
 # loop
-varlist <- unique(BMpops.long$time) #19 variables
+varlist <- unique(BMpops.long$time) #21 variables
 
 i=1
 for(i in 1:length(varlist)) {
